31cf1779078b47500fc823c73c4ec7c2a53eeb1d
ToobaBukhari <140112749+ToobaBukhari@users.noreply.github.com>
2023-10-10 04:39:05 +0500

    Question21 to 30 solution

---
+++ Question22.ts
@@ -0,0 +1,16 @@
+/* Intentional Error: If you haven’t received an array index error in one of your programs
+ yet, try to make one happen. Change an index in one of your programs to produce an index error.
+ Make sure you correct the error before closing the program.*/
+
+ let BTS_Band_Members:string[]=[
+    "RM",
+    "Jhope",
+    "Suga",
+    "Jin",
+    "Jimin",
+    "V",
+    "Jungkook"
+    ]
+    //index eror
+    console.log(BTS_Band_Members[7]);//undefined
+    console.log(BTS_Band_Members[5]);
